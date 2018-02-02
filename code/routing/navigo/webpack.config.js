baa1e9a34458ee2424c56f2dcab6bb2bb8451964
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-02-02 10:22:59 +0100

    added navigo routing example

---
+++ code/routing/navigo/webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [ {
+          loader: 'html-loader'
+        }]
+      },
+      {
+        test: /\.svg$/,
+        use: [ 'url-loader?mimetype=image/svg+xml' ]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
+}
